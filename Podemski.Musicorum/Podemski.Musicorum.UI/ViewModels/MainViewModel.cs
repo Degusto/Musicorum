378e1743a8f95e04523fb4cb44b82b5c5af8b245
Degusto <test@qwewq321321fdsgfswea43q43wd.pl>
2018-11-14 14:47:35 +0100

    Dodano dynamiczne wyszukiwanie

--- Podemski.Musicorum/Podemski.Musicorum.UI/ViewModels/MainViewModel.cs
+++ Podemski.Musicorum/Podemski.Musicorum.UI/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 
+using Podemski.Musicorum.Core.Enums;
 using Podemski.Musicorum.Interfaces.Entities;
 using Podemski.Musicorum.Interfaces.SearchCriterias;
 using Podemski.Musicorum.Interfaces.Services;
@@ -11,6 +12,8 @@ namespace Podemski.Musicorum.UI.ViewModels
 {
     public sealed class MainViewModel : ViewModelBase
     {
+        private readonly SearchCriteria _searchCriteria;
+
         private readonly IViewService _viewService;
         private readonly IArtistService _artistService;
         private readonly IAlbumService _albumService;
@@ -23,7 +26,7 @@ namespace Podemski.Musicorum.UI.ViewModels
             _albumService = albumService;
             _trackService = trackService;
 
-            SearchCriteria = new SearchCriteria();
+            _searchCriteria = new SearchCriteria();
         }
 
         public RelayCommand<IArtist> OpenArtistCommand => new RelayCommand<IArtist>(ShowView, x => x != null);
@@ -34,13 +37,50 @@ namespace Podemski.Musicorum.UI.ViewModels
 
         public RelayCommand SearchCommand => new RelayCommand(Search);
 
-        public SearchCriteria SearchCriteria { get; }
+        public IEnumerable<IArtist> Artists => _artistService.Find(_searchCriteria);
 
-        public IEnumerable<IArtist> Artists => _artistService.Find(SearchCriteria);
+        public IEnumerable<IAlbum> Albums =>  _albumService.Find(_searchCriteria);
 
-        public IEnumerable<IAlbum> Albums =>  _albumService.Find(SearchCriteria);
+        public IEnumerable<ITrack> Tracks => _trackService.Find(_searchCriteria);
 
-        public IEnumerable<ITrack> Tracks => _trackService.Find(SearchCriteria);
+        public string Name
+        {
+            get => _searchCriteria.Name;
+            set
+            {
+                _searchCriteria.Name = value;
+
+                RaisePropertyChanged(() => Name);
+
+                Search();
+            }
+        }
+
+        public Genre Genre
+        {
+            get => _searchCriteria.Genre;
+            set
+            {
+                _searchCriteria.Genre = value;
+
+                RaisePropertyChanged(() => Genre);
+
+                Search();
+            }
+        }
+
+        public AlbumVersion AlbumVersion
+        {
+            get => _searchCriteria.AlbumVersion;
+            set
+            {
+                _searchCriteria.AlbumVersion = value;
+
+                RaisePropertyChanged(() => AlbumVersion);
+
+                Search();
+            }
+        }
 
         private void ShowView(IEntity entity)
         {
